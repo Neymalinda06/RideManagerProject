03c8e87477f97453cef4573dfcdf21c532dd734b
AsithD <131624617+AsithD@users.noreply.github.com>
2024-09-25 20:42:10 +1000

    Update Visitor.java
    A new instance variable called "notes" has been added. Further, it has been implemented as optional so if "notes" is empty, it will be saved as null

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,6 +1,7 @@
 public class Visitor extends Person {
     private String visitorId;
     private String ticketType;
+    private String notes;
 
     public Visitor() {
         this.setName(null);
@@ -8,6 +9,16 @@ public class Visitor extends Person {
         this.setGender(null);
         this.setVisitorId(null);
         this.setTicketType(null);
+        this.setNotes(null);
+    }
+
+    public Visitor(String name, int age, String gender, String visitorId, String ticketType, String n) {
+        this.setName(name);
+        this.setAge(age);
+        this.setGender(gender);
+        this.setVisitorId(visitorId);
+        this.setTicketType(ticketType);
+        this.setNotes(n);
     }
 
     public Visitor(String name, int age, String gender, String visitorId, String ticketType) {
@@ -16,6 +27,7 @@ public class Visitor extends Person {
         this.setGender(gender);
         this.setVisitorId(visitorId);
         this.setTicketType(ticketType);
+        this.setNotes(null);
     }
 
     public String getVisitorId() {
@@ -33,9 +45,21 @@ public class Visitor extends Person {
     public void setTicketType(String ticketType) {
         this.ticketType = ticketType;
     }
+
+    public String getNotes()
+    {
+        return notes;
+    }
+
+    public void setNotes(String notes)
+    {
+        this.notes = notes;
+    }
+
+   
     @Override
     public String Details() {
-        return "Visitor, name: " + getName() + ", age: " + getAge() + ", gender: " + getGender() + ", id: " + getVisitorId() + ", ticket type: " + getTicketType();
+        return "Visitor, name: " + getName() + ", age: " + getAge() + ", gender: " + getGender() + ", NOTES :" + getNotes();
     }
 
 }
